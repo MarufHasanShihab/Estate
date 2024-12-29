19d0c57e0df430609dd2fdeab47e7b3f64bac763
Maruf Hasan Shihab <devmarufhasan@gmail.com>
2024-12-29 22:49:52 +0600

    added projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./Components/About/About";
 import Header from "./Components/Header/Header";
+import Projects from "./Components/Projects/Projects";
 
 function App() {
   return (
     <div className="w-full overflow-hidden">
       <Header />
       <About/>
+      <Projects/>
     </div>
   );
 }
--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types"
 
 const Title = ({title,subTitle,description}) => {
     return (
-        <div>
+        <div className="text-center">
             <h1 className="text-2xl sm:text-4xl font-bold mb-2">{title} <span className="underline underline-offset-4 decoration-1 under font-light">{subTitle}</span></h1>
-            <p className="text-gray-500 max-w-80 text-center mb-8">{description}</p>
+            <p className="text-gray-500 max-w-80 mx-auto text-center mb-8">{description}</p>
         </div>
     );
 };
